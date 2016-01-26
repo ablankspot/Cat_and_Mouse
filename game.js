d408781633482259b99f1c82611e0ea2af792ddf
ablankspot <fiatlux00x@hotmail.com>
2016-01-25 19:00:12 -0800

    Add Placeholders for Background, Logo and buttons of the start screen

--- game.js
+++ game.js
@@ -27,21 +27,74 @@ function DrawStart()
     if (canvas != null)
     {
         var ctx = canvas.getContext("2d");
-        var x_0 = (canvas_w / 2) - 150;
-        var y_0 = (canvas_h / 2) - 50;
+        var w = (canvas_w / 2);
+        var h = (canvas_h / 2);
+        var button_w = Math.ceil((w / 10) * 8);
+        var button_h = Math.ceil((h / 10) * 5);
+        var x_0, y_0;
 
+        // Draw the background placeholder
+        ctx.globalAlpha = 0.2;
         ctx.beginPath();
-        ctx.rect(x_0, y_0, 300, 100);
+        ctx.rect(0, 0, canvas_w, canvas_h);
+        ctx.fillStyle="#00ff00";
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+
+        // Draw the Logo placeholder
+        ctx.globalAlpha = 0.3;
+        ctx.beginPath();
+        ctx.rect(0, 0, canvas_w, canvas_h / 2);
+        ctx.fillStyle="#0000ff";
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath(); 
+
+        ctx.globalAlpha = 1.0;
+        ctx.beginPath();
+        ctx.font = "80px Poiret One";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.fillText("LOGO", w, h / 2);
+        ctx.closePath();       
+
+        // Draw the 'Play' button placeholder
+        x_0 = Math.ceil(w / 10);
+        y_0 = Math.ceil((h / 10) * 2.5) + h;
+        ctx.beginPath();
+        ctx.rect(x_0, y_0, button_w, button_h);
         ctx.fillStyle="#ffffff";
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
 
+        x_0 = (x_0 + button_w) - (button_w / 2);
+        y_0 = y_0 + (button_h / 2);
         ctx.beginPath();
-        ctx.font = "40px Poiret One";
+        ctx.font = "80px Poiret One";
         ctx.fillStyle = "red";
         ctx.textAlign = "center";
-        ctx.fillText("Start Game", canvas_w/2, canvas_h/2 + 15);
+        ctx.fillText("Play", x_0, y_0);
+        ctx.closePath();
+
+        // Draw the 'High scores' button placeholder
+        x_0 = Math.ceil(w / 10) + w;
+        y_0 = Math.ceil((h / 10) * 2.5) + h;
+        ctx.beginPath();
+        ctx.rect(x_0, y_0, button_w, button_h);
+        ctx.fillStyle="#ffffff";
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+
+        x_0 = (x_0 + button_w) - (button_w / 2);
+        y_0 = y_0 + (button_h / 2);
+        ctx.beginPath();
+        ctx.font = "80px Poiret One";
+        ctx.fillStyle = "red";
+        ctx.textAlign = "center";
+        ctx.fillText("High Scores", x_0, y_0);
         ctx.closePath();
 
         //window.onkeydown = KeyStrokeDown;
